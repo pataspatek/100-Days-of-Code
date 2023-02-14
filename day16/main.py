43b3524cc2b1f1a76bc3f2a5472f957e0106b9bb
pataspatek <patrikpatek3@gmail.com>
2023-02-14 14:27:48 +0100

    day16 completed

---
+++ day16/main.py
@@ -0,0 +1,37 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+def main():
+    menu = Menu()
+    coffee_maker = CoffeeMaker()
+    money_machine = MoneyMachine()
+
+    machine_running = True
+    while machine_running:
+
+        # Available drinks
+        options = menu.get_items()
+
+        choice = input(f"What would you like? ({options}): ")
+
+        # Show the report of the coffee machine
+        if choice == "report":
+            coffee_maker.report()
+            money_machine.report()
+
+        # Secret command for turnning off the machine
+        elif choice == "off":
+            machine_running = False
+            print("Shutting down...")
+
+        else:
+            drink = menu.find_drink(choice)
+            if coffee_maker.is_resource_sufficient(drink):
+                if money_machine.make_payment(drink.cost):
+                    coffee_maker.make_coffee(drink)
+
+
+if __name__ == "__main__":
+    main()
